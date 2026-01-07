f9dee2f9414f4e6667004222db6397add31ef6bf
etienne-hd <hode.etienne@gmail.com>
2026-01-07 17:32:05 +0100

    Added: explicit message when no argument is provided

--- src/cub3d.c
+++ src/cub3d.c
@@ -1,9 +1,13 @@
+#include "ft_printf.h"
 #include "parsing.h"
 
 int	main(int argc, char **argv)
 {
 	if (argc != 2)
+	{
+		ft_dprintf(2, "Usage: %s <*.cub>\n", argv[0]);
 		return (2);
+	}
 	parse(argv[1]);
 	return (0);
 }
