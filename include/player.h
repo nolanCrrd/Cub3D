8d9045c6109e4676f85a077473013341145e97ae
nolanCrrd <nolan.crrd@gmail.com>
2026-01-08 15:17:57 +0100

    Changed: position and direction in player

--- include/player.h
+++ include/player.h
@@ -1,11 +1,16 @@
 #ifndef PLAYER_H
 # define PLAYER_H
 
+# define PLAYER_BASE_SPEED 10;
+
+#define	X 0;
+#define Y 1;
+
 typedef struct s_player
 {
-	double	x;
-	double	y;
-	double	direction;
+	double	pos[2];
+	double	direction_angle;
+	double	direction_vec[2];
 	double	speed;
 }	t_player;
 
