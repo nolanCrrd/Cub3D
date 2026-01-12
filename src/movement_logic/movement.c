ab3caaa93417b21de67d90eaa56406846527c1a3
nolanCrrd <nolan.crrd@gmail.com>
2026-01-12 17:24:46 +0100

    Modified: collision accept system

--- src/movement_logic/movement.c
+++ src/movement_logic/movement.c
@@ -1,45 +1,47 @@
 #include "ctx.h"
+#include "libft.h"
 
 // TODO: Probleme de colision chelou
 void	player_move(t_ctx *ctx)
 {
 	double	move_speed;
+	const char	*walkable = "0O";
 
 	move_speed = ctx->frame_time / 1000.0 * ctx->player->speed;
 	if (ctx->player->movement[0] && !ctx->player->movement[1])
 	{
-		if (ctx->map->grid[(int)ctx->player->pos[Y]][(int)(ctx->player->pos[X]
-			+ ctx->player->dir_vec[X] * move_speed)] == '0')
+		if (ft_strchr(walkable, ctx->map->grid[(int)ctx->player->pos[Y]][(int)(ctx->player->pos[X]
+			+ ctx->player->dir_vec[X] * move_speed)]))
 			ctx->player->pos[X] += ctx->player->dir_vec[X] * move_speed;
-		if (ctx->map->grid[(int)(ctx->player->pos[Y] + ctx->player->dir_vec[Y]
-			* move_speed)][(int)(ctx->player->pos[X])] == '0')
+		if (ft_strchr(walkable, ctx->map->grid[(int)(ctx->player->pos[Y] + ctx->player->dir_vec[Y]
+			* move_speed)][(int)(ctx->player->pos[X])]))
 			ctx->player->pos[Y] += ctx->player->dir_vec[Y] * move_speed;
 	}
 	if (!ctx->player->movement[0] && ctx->player->movement[1])
 	{
-		if (ctx->map->grid[(int)ctx->player->pos[Y]][(int)(ctx->player->pos[X]
-			- ctx->player->dir_vec[X] * move_speed)] == '0')
+		if (ft_strchr(walkable, ctx->map->grid[(int)ctx->player->pos[Y]][(int)(ctx->player->pos[X]
+			- ctx->player->dir_vec[X] * move_speed)]))
 			ctx->player->pos[X] -= ctx->player->dir_vec[X] * move_speed;
-		if (ctx->map->grid[(int)(ctx->player->pos[Y] - ctx->player->dir_vec[Y]
-			* move_speed)][(int)(ctx->player->pos[X])] == '0')
+		if (ft_strchr(walkable, ctx->map->grid[(int)(ctx->player->pos[Y] - ctx->player->dir_vec[Y]
+			* move_speed)][(int)(ctx->player->pos[X])]))
 			ctx->player->pos[Y] -= ctx->player->dir_vec[Y] * move_speed;
 	}
 	if (ctx->player->movement[2] && !ctx->player->movement[3])
 	{
-		if (ctx->map->grid[(int)ctx->player->pos[Y]][(int)(ctx->player->pos[X]
-			+ ctx->player->dir_vec[Y] * move_speed)] == '0')
+		if (ft_strchr(walkable, ctx->map->grid[(int)ctx->player->pos[Y]][(int)(ctx->player->pos[X]
+			+ ctx->player->dir_vec[Y] * move_speed)]))
 			ctx->player->pos[X] += ctx->player->dir_vec[Y] * move_speed;
-		if (ctx->map->grid[(int)(ctx->player->pos[Y] - ctx->player->dir_vec[X]
-			* move_speed)][(int)(ctx->player->pos[X])] == '0')
+		if (ft_strchr(walkable, ctx->map->grid[(int)(ctx->player->pos[Y] - ctx->player->dir_vec[X]
+			* move_speed)][(int)(ctx->player->pos[X])]))
 			ctx->player->pos[Y] -= ctx->player->dir_vec[X] * move_speed;
 	}
 	if (!ctx->player->movement[2] && ctx->player->movement[3])
 	{
-		if (ctx->map->grid[(int)ctx->player->pos[Y]][(int)(ctx->player->pos[X]
-			- ctx->player->dir_vec[Y] * move_speed)] == '0')
+		if (ft_strchr(walkable, ctx->map->grid[(int)ctx->player->pos[Y]][(int)(ctx->player->pos[X]
+			- ctx->player->dir_vec[Y] * move_speed)]))
 			ctx->player->pos[X] -= ctx->player->dir_vec[Y] * move_speed;
-		if (ctx->map->grid[(int)(ctx->player->pos[Y] + ctx->player->dir_vec[X]
-			* move_speed)][(int)(ctx->player->pos[X])] == '0')
+		if (ft_strchr(walkable, ctx->map->grid[(int)(ctx->player->pos[Y] + ctx->player->dir_vec[X]
+			* move_speed)][(int)(ctx->player->pos[X])]))
 			ctx->player->pos[Y] += ctx->player->dir_vec[X] * move_speed;
 	}
 }
