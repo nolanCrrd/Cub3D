1ea128df7dc409cb7e4fd4d3d48ce9755a843116
nolanCrrd <nolan.crrd@gmail.com>
2026-01-15 13:03:00 +0100

    Added: FPS counter on screen

--- src/render/update_bonus.c
+++ src/render/update_bonus.c
@@ -15,6 +15,7 @@
 #include "player.h"
 #include "render.h"
 #include "utils.h"
+#include "libft.h"
 
 static void	render_door_text(t_ctx *ctx)
 {
@@ -55,5 +56,8 @@ void	update(void *ptr)
 	mlx_put_image_to_window(ctx->mlx, ctx->win, ctx->render, 0, 0);
 	display_map(ctx);
 	display_rec(ctx);
+	if (ctx->frame_time > 0)
+		mlx_string_put(ctx->mlx, ctx->win, 20, 35,
+			(mlx_color){.rgba = 0xA10000ff}, ft_itoa(1000 / ctx->frame_time));
 	render_door_text(ctx);
 }
